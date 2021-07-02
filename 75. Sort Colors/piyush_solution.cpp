c47af55f964807f5cd673d4edc2e5251917a9a33
piyushatri7 <piyushatri7@gmail.com>
2021-07-03 00:34:50 +0530

    Create piyush_solution.cpp

---
+++ 75. Sort Colors/piyush_solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int c1=0,c2=0,c3=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)  c1++;
+            if(nums[i]==1)  c2++;
+            if(nums[i]==2)  c3++;
+        }
+        
+        int i=0;
+        while(c1--)    nums[i++]=0;
+        while(c2--)    nums[i++]=1;
+        while(c3--)    nums[i++]=2;
+    }
+};
