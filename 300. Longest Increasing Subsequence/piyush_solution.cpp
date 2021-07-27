8ab88d936ce1e0f285fcfbafb512cc63f6f9a86e
piyushatri7 <piyushatri7@gmail.com>
2021-07-27 22:28:32 +0530

    Create piyush_solution.cpp

---
+++ 300. Longest Increasing Subsequence/piyush_solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> res;
+        for(auto x:nums)
+        {
+            auto it = lower_bound(res.begin(),res.end(),x);
+            if(it==res.end())
+                res.push_back(x);
+            else
+                *it = x;
+        }
+        return res.size();
+    }
+};
