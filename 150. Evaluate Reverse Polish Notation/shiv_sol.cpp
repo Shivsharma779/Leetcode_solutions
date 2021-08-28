eba9c12637934e6eab37e9d38f258e8bd594dbed
Shiv <42580368+Shivsharma779@users.noreply.github.com>
2021-08-28 12:23:21 +0530

    Create shiv_sol.cpp

---
+++ 150. Evaluate Reverse Polish Notation/shiv_sol.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+   
+    int evalRPN(vector<string>& tokens) {
+        stack<int> stk;
+        for(auto s:tokens) {
+            if(s.size()>1 ||isdigit(s[0])) 
+                stk.push(stoi(s));
+            else {
+                int x2=stk.top();
+                stk.pop();
+                int x1=stk.top();
+                stk.pop();
+                switch(s[0]) {
+                    case '+': x1+=x2; break;
+                    case '-': x1-=x2; break;
+                    case '*': x1*=x2; break;
+                    case '/': x1/=x2; break;
+                }
+                stk.push(x1);
+            }
+        }
+        return stk.top();        
+    }
+};
