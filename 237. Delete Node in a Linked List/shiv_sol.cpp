a6b4d4adb0879d731f92ac3b1aaa5b2db311aee0
Shiv <42580368+Shivsharma779@users.noreply.github.com>
2021-09-01 10:53:53 +0530

    Create shiv_sol.cpp

---
+++ 237. Delete Node in a Linked List/shiv_sol.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        
+        ListNode * t = node -> next;
+        node->val = t->val;
+        node->next = t->next;
+        // delete(t);
+        
+        
+    }
+};
